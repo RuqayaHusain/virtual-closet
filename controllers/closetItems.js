c063b48c4d737e5b0ac23e155a732fc52b172e19
Ruqaya Husain <ruqayanader22@gmail.com>
2025-09-28 10:02:37 +0300

    added edit page + route, removed unwanted option from the new form

--- controllers/closetItems.js
+++ controllers/closetItems.js
@@ -53,4 +53,35 @@ router.get('/:itemId', async (req, res) => {
     }
 });
 
+router.get('/:itemId/edit', async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+        const selectedItem = await ClosetItem.findById(itemId).populate('owner');
+        res.render('closetItems/edit.ejs', {
+            selectedItem,
+        });
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
+router.put('/:itemId', async (req, res) => {
+    try {
+        const itemId = req.params.itemId;
+        const selectedItem = await ClosetItem.findById(itemId).populate('owner');
+
+        if (selectedItem.owner._id.equals(req.session.user._id)) {
+            await selectedItem.updateOne(req.body);
+            res.redirect(`/closetItems/${itemId}`);
+        } else {
+            console.log('permission denied!');
+            res.redirect('/');
+        }
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
+});
+
 module.exports = router;
